f400a06d22225224d70c4729c31fb404a683b6b6
StanFromIreland <stanulbrych@gmail.com>
2025-01-02 10:28:05 +0000

    Misc. Minor work

--- app.py
+++ app.py
@@ -5,9 +5,18 @@ app = flask.Flask(__name__, template_folder='templates')
 
 
 @app.route("/")
-def index():
+def home():
     return render_template('index.html')
+
+
+@app.route("/projects/")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/projects/website/")
+def projects_website():
+    return render_template('website.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
